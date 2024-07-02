1142b8b34aa05eb9cef4ffbac568bfe3333bc000
vallisrisista <vallisrisista0909@gmail.com>
2024-07-02 22:05:03 +0530

    Simple implementation of collection api

---
+++ CollectionDemo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class CollectionDemo {
+       public static void main(String[] args) {
+            Collection nums=new ArrayList<>();
+            nums.add(2);
+            nums.add("d");
+            System.out.println(nums);
+            
+    }
+
+}
