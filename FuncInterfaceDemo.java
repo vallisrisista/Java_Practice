54b98361fb260b6a5e8f28bffb83e1834d5754a1
vallisrisista <vallisrisista0909@gmail.com>
2024-07-07 12:45:21 +0530

    Lambda Expression

--- FuncInterfaceDemo.java
+++ FuncInterfaceDemo.java
@@ -7,15 +7,10 @@ interface Data {
 }
 class FuncInterfaceDemo{
     public static void main(String[] args) {
-        Data d=new Data(){
-            /* below is directly implementing the abstract method in Data 
-            while creating an object of it. Else, obj cant be created to Data
-            as it is an interface. This can be done only if data is a 
-            functional interface */ 
-            public void process(){
-                System.out.println("Data Processed");
-            }
-        };
+        Data d=()->{
+            //Optimized code for func interfacing using lambda exp
+            System.out.println("Data Processed");
+             };
         d.process();
         
     }
