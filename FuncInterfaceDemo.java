f9d6e53fbffc6dacf37f0f41da5db0cacd54b427
vallisrisista <vallisrisista0909@gmail.com>
2024-07-07 12:37:48 +0530

    functional interface

--- FuncInterfaceDemo.java
+++ FuncInterfaceDemo.java
@@ -7,15 +7,10 @@ interface Data {
 }
 class FuncInterfaceDemo{
     public static void main(String[] args) {
-        Data d=new Data(){
-            /* below is directly implementing the abstract method in Data 
-            while creating an object of it. Else, obj cant be created to Data
-            as it is an interface. This can be done only if data is a 
-            functional interface */ 
-            public void process(){
+        //optimized implementation of func interface using lambda expression
+        Data d= () ->  {
                 System.out.println("Data Processed");
-            }
-        };
+            };
         d.process();
         
     }
